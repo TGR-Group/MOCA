1aa07d1bd5cdcc422a1933eacb58b94d0e1dab9f
toporone <157017292+toporone@users.noreply.github.com>
2024-06-24 20:55:35 +0900

    現在までのデータ

--- MOCA/backend/init_db.py
+++ MOCA/backend/init_db.py
@@ -3,27 +3,31 @@ from psycopg2 import sql
 from dotenv import load_dotenv
 import os
 
-def create_database():
-    load_dotenv()
+load_dotenv()
 
+def create_database():
     user = os.getenv('PG_ADMIN_USER')
     password = os.getenv('PG_ADMIN_PASSWORD')
     host = os.getenv('PG_HOST')
-    port = os.getenv('PG_PORT')
-
-    new_user = os.getenv('DB_USER')
-    new_password = os.getenv('DB_PASSWORD')
-    new_db = os.getenv('DB_NAME')
+    port = os.getenv('PG_PORT',5432)
 
     conn = psycopg2.connect(dbname='postgres', user=user, password=password, host=host, port=port)
+    conn.set_client_encoding('UTF8')
     conn.autocommit = True
-    cursor = conn.cursor()
+    cur = conn.cursor()
 
-    cursor.execute(sql.SQL("CREATE USER {} WITH PASSWORD %s").format(sql.Identifier(new_user)), [new_password])
+    cur.execute("SELECT 1 FROM pg_database WHERE datname = %s", (os.getenv('DB_NAME'),))
+    exists = cur.fetchone()
+    if not exists:
+        cur.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier(os.getenv('DB_NAME'))))
 
-    cursor.execute(sql.SQL("CREATE DATABASE {} OWNER {}").format(sql.Identifier(new_db), sql.Identifier(new_user)))
+    cur.execute("SELECT 1 FROM pg_roles WHERE rolname = %s", (os.getenv('DB_USER'),))
+    exists = cur.fetchone()
+    if not exists:
+        cur.execute(sql.SQL("CREATE USER {} WITH PASSWORD %s").format(sql.Identifier(os.getenv('DB_USER'))), (os.getenv('DB_PASSWORD'),))
+        cur.execute(sql.SQL("GRANT ALL PRIVILEGES ON DATABASE {} TO {}").format(sql.Identifier(os.getenv('DB_NAME')), sql.Identifier(os.getenv('DB_USER'))))
 
-    cursor.close()
+    cur.close()
     conn.close()
 
 if __name__ == '__main__':
